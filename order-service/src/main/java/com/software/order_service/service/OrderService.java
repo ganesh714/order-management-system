e04952502321d08b4e78b000d3c5e8f129fb276e
ganesh714 <evvganesh1@gmail.com>
2026-03-12 23:21:02 +0530

    feat: implement OrderService with core business logic

---
+++ order-service/src/main/java/com/software/order_service/service/OrderService.java
@@ -0,0 +1,31 @@
+package com.software.order_service.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.software.order_service.model.Order;
+import com.software.order_service.model.OrderState;
+import com.software.order_service.repository.OrderRepository;
+
+@Service
+public class OrderService {
+    @Autowired
+    OrderRepository orderRepository;
+
+    public Order createOrder(Order order) {
+        order.setState(OrderState.ORDER_CREATED);
+        Order savedOrder = orderRepository.save(order);
+        // TODO : State macine
+        return savedOrder;
+    }
+
+    public Order getOrderByID(String id) {
+        return orderRepository.getReferenceById(id);
+    }
+
+    public void updateOrderState(String id, OrderState state) {
+        Order order = getOrderByID(id);
+        order.setState(state);
+        orderRepository.save(order);
+    }
+}

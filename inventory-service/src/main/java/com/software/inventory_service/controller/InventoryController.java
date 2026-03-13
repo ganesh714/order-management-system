4529350a08689b4f52359b14bcfb0bd73da4fd4e
ganesh714 <evvganesh1@gmail.com>
2026-03-14 01:39:54 +0530

    refactor: map InventoryController explicitly to /api/inventory

--- inventory-service/src/main/java/com/software/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/software/inventory_service/controller/InventoryController.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/inventory")
 public class InventoryController {
 	
 	@Value("${fail.on.order.id}")
 	private String failOnOrderId;
 	
-	@PostMapping("/inventory")
+	@PostMapping
 	public ResponseEntity<String> reserveInventory(@RequestParam String orderId){
 		System.out.println("Received inventory reservation request for Order: " + orderId);
 		
@@ -23,7 +25,7 @@ public class InventoryController {
 					.body("Inventory reservation failed: Out of stock.");
 		}
 		System.out.println("Inventory reserved successfully for Order: " + orderId);
-		return ResponseEntity.ok("Inventory reserved.");
+		return ResponseEntity.ok("Inventory reserved.x");
 		
 	}
 }

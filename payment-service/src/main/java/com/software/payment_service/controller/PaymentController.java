a179d74dd136a98041789f2b658f000fd17cca54
ganesh714 <evvganesh1@gmail.com>
2026-03-14 01:40:24 +0530

    refactor: map PaymentController explicitly to /api/payments

--- payment-service/src/main/java/com/software/payment_service/controller/PaymentController.java
+++ payment-service/src/main/java/com/software/payment_service/controller/PaymentController.java
@@ -6,16 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/payments")
 public class PaymentController {
 
     @Value("${fail.on.order.id}")
     private String failOnOrderId;
 
-    @PostMapping("/payments")
+    @PostMapping
     public ResponseEntity<String> processPayment(@RequestBody String orderId) {
         System.out.println("Received payment request for Order: " + orderId);
 

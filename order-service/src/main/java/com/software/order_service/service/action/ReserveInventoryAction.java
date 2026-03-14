0d34243a1359c37a38a4ac0654fd360a2410fea0
ganesh714 <evvganesh1@gmail.com>
2026-03-14 21:42:14 +0530

    refactor: enhance reliability of event firing in ReserveInventoryAction

--- order-service/src/main/java/com/software/order_service/service/action/ReserveInventoryAction.java
+++ order-service/src/main/java/com/software/order_service/service/action/ReserveInventoryAction.java
@@ -4,12 +4,14 @@ import com.software.order_service.model.OrderEvent;
 import com.software.order_service.model.OrderState;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Mono;
+import java.time.Duration;
 
 @Component
 public class ReserveInventoryAction implements Action<OrderState, OrderEvent> {
@@ -44,7 +46,10 @@ public class ReserveInventoryAction implements Action<OrderState, OrderEvent> {
     }
 
     private void sendEvent(StateContext<OrderState, OrderEvent> context, OrderEvent event, String orderId) {
-        context.getStateMachine().sendEvent(Mono.just(MessageBuilder.withPayload(event)
-                .setHeader("orderId", orderId).build())).subscribe();
+        Mono.delay(Duration.ofMillis(200)).subscribe(t -> {
+            System.out.println("🚀 Triggering internal event: " + event + " on Order: " + orderId);
+            Message<OrderEvent> msg = MessageBuilder.withPayload(event).setHeader("orderId", orderId).build();
+            context.getStateMachine().sendEvent(Mono.just(msg)).subscribe();
+        });
     }
 }
